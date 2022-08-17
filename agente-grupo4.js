3df68751433cc0d892a6c94c628b069f8bed1ffe
Pedro Nogueira COutinho <pedro.nogueira@neurotech.com.br>
2022-08-16 21:58:58 -0300

    A* implemented

--- agente-grupo4.js
+++ agente-grupo4.js
@@ -1,5 +1,25 @@
 //let previous;
 
+class Heap{
+  constructor() {
+    //list = [ [Vector(i, j), cost] ]
+    this.list = Array(0);
+  }
+  fsort(a, b) {
+    return a[1] > b[1];
+  }
+  pop() {
+    this.list.sort(this.fsort);
+    return this.list.pop();
+  }
+  push(a) {
+    this.list.push(a);
+  }
+  empty() {
+    return this.list.length === 0;
+  }
+  
+};
 
 class AgenteGrupo4 {
   constructor(position) {
@@ -11,6 +31,8 @@ class AgenteGrupo4 {
     this.dx = [1,-1,0, 0, 1, 1,-1,-1];
     this.dy = [0, 0,1,-1, 1,-1, 1,-1];
     this.terrainEnergy=0;
+
+    
   }
   
   agentPixelToGrid(position) {
@@ -32,11 +54,11 @@ class AgenteGrupo4 {
         this.pathAlgorithm += 1;
         let gridPosition = this.pixelToGrid(this.position);
         this.terrainEnergy = (10*terrain.board[gridPosition.x][gridPosition.y])+5;
-        console.log(this.terrainEnergy);
+        //console.log(this.terrainEnergy);
         
         this.calculatedPath = Array();
 
-        this.dfs(gridPosition, terrain);
+        this.astar(gridPosition, terrain);
       
         // switch(algorithmToRun) {
         //   case 0:
@@ -62,13 +84,15 @@ class AgenteGrupo4 {
       // console.log(this.closestFood);
       // this.display();
       // exit(-1);
+      
+      
     }
     
     
     if(this.terrainEnergy <=0){
       let actualPosition = this.pixelToGrid(this.position);
       this.terrainEnergy = (10*terrain.board[actualPosition.x][actualPosition.y]) + 5;
-      console.log(this.terrainEnergy);
+      //console.log(this.terrainEnergy);
       
       this.update();
     }
@@ -181,8 +205,68 @@ class AgenteGrupo4 {
     
   }
   
-  astar(terrain) {
+
+  astarAux(gridPosition, terrain, dist, ancestor){
+    dist[gridPosition.x][gridPosition.y] = 0;
+
+    let pq = new Heap();
+
+    pq.push([gridPosition, 0]);
+
+    while(!pq.empty()){
+      let currPosition;
+      let cost;
+      [currPosition, cost] = pq.pop();
+        
+      for(let i = 0; i < 8; i++){
+        let new_x = currPosition.x + this.dx[i];
+        let new_y = currPosition.y + this.dy[i];
+        let c = 0;
+        if(this.dx[i] !== 0 && this.dy[i] !== 0) c = 1;
+
+        if(!(new_x >= 0 && new_x < terrain.columns && new_y >= 0 && new_y < terrain.rows)) continue;
+        if(terrain.board[new_x][new_y] === OBSTACLE) continue;
+        
+        let newDist = cost + c + 1 + terrain.board[new_x][new_y];
+        if(dist[new_x][new_y] === -1 || newDist < dist[new_x][new_y]) {
+          dist[new_x][new_y] = newDist;
+          ancestor[new_x][new_y] = currPosition;
+          pq.push([createVector(new_x, new_y), newDist]);
+        }
+          
+      }
+    }
     
+    let initialPosition = this.pixelToGrid(this.closestFood);
+
+    while(!initialPosition.equals(ancestor[initialPosition.x][initialPosition.y])) {
+      this.calculatedPath.push(this.gridToPixel(initialPosition));
+      
+      initialPosition = createVector(ancestor[initialPosition.x][initialPosition.y].x, ancestor[initialPosition.x][initialPosition.y].y);
+    }
+    this.calculatedPath.push(this.gridToPixel(initialPosition));
+
+    this.calculatedPath = reverse(this.calculatedPath);
+  }
+
+  astar(gridPosition, terrain) {
+    let dist = new Array(terrain.columns);
+    for(let i = 0; i < terrain.columns; i++){
+      dist[i] = new Array(terrain.rows);
+      for(let j = 0; j < terrain.rows; j++){
+        dist[i][j] = -1;
+      }
+    }
+    let ancestor = new Array(terrain.columns);
+    for (let i = 0; i < terrain.columns; i++) {
+      ancestor[i] = new Array(terrain.rows);
+    }
+    for(let i = 0; i < terrain.columns; i++){
+      for(let j = 0; j < terrain.rows; j++){
+        ancestor[i][j] = createVector(i,j);
+      }
+    }
+    this.astarAux(gridPosition, terrain, dist, ancestor);
   }
   
   uniform(terrain) {
